70cb20d721fa4f1fd763cc2129d66ede43115fcf
chenpi <641760512@qq.com>
2022-05-18 17:11:44 +0800

    测试用例分组执行、跳过执行

--- cases/test_login.py
+++ cases/test_login.py
@@ -1,22 +1,35 @@
 import pytest
 
 
-def test_fix():
-    print("测试函数")
+def test_fi01():
+    print("测试函数01")
     assert 1 == 2
 
 
 class TestLogin:
+    age = 18
 
-    @pytest.mark.smoke
+    # 标记执行顺序
+    @pytest.mark.run(order=1)
     def test_01(self):
-        print("登录测试方法01")
-    @pytest.mark.usermanage
+        print("测试方法01")
+
+    # 标记成冒烟用例
+    @pytest.mark.smoke
     def test_02(self):
-        print("登录测试方法02")
+        print("测试方法02")
 
+    # 标记成模块用例
+    @pytest.mark.usermanage
     def test_03(self):
-        print("登录测试方法03")
+        print("测试方法03")
 
+    # 标记成无条件跳过
+    @pytest.mark.skip(reason="还没调试好")
     def test_04(self):
-        print("登录测试方法04")
+        print("测试方法04")
+
+    # 标记成有条件跳过
+    @pytest.mark.skipif(age >= 18, reason="已成年，不执行")
+    def test_05(self):
+        print("测试方法05")
