9835b4efe8fb4560ba4f9cc4f1a439116ec11e0a
chenpi <641760512@qq.com>
2022-05-18 15:02:54 +0800

    运行指定mark测试用例

--- cases/test_login.py
+++ cases/test_login.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_fix():
     print("测试函数")
     assert 1 == 2
@@ -5,15 +8,15 @@ def test_fix():
 
 class TestLogin:
 
+    @pytest.mark.smoke
     def test_01(self):
         print("登录测试方法01")
-
+    @pytest.mark.usermanage
     def test_02(self):
         print("登录测试方法02")
 
     def test_03(self):
         print("登录测试方法03")
-        assert 1 == 2
 
     def test_04(self):
         print("登录测试方法04")
