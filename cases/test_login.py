b14e8463fe18f0bbb172cac2844513973f6bb8db
chenpi <641760512@qq.com>
2022-05-18 14:06:09 +0800

    初始化修改

---
+++ cases/test_login.py
@@ -0,0 +1,19 @@
+def test_fix():
+    print("测试函数")
+    assert 1 == 2
+
+
+class TestLogin:
+
+    def test_01(self):
+        print("登录测试方法01")
+
+    def test_02(self):
+        print("登录测试方法02")
+
+    def test_03(self):
+        print("登录测试方法03")
+        assert 1 == 2
+
+    def test_04(self):
+        print("登录测试方法04")
